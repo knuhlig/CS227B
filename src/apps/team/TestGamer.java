910ca81882101e37a4644552ba390b5b7963d965
Karl Uhlig <knuhlig@stanford.edu>
2012-04-18 21:30:29 -0700

    added a test gamer that copies the 'first legal move' strategy

---
+++ src/apps/team/TestGamer.java
@@ -0,0 +1,60 @@
+package apps.team;
+
+import java.util.List;
+
+import player.gamer.statemachine.StateMachineGamer;
+import player.gamer.statemachine.reflex.event.ReflexMoveSelectionEvent;
+import player.gamer.statemachine.reflex.gui.ReflexDetailPanel;
+import util.statemachine.Move;
+import util.statemachine.StateMachine;
+import util.statemachine.exceptions.MoveDefinitionException;
+import util.statemachine.implementation.prover.ProverStateMachine;
+import apps.player.detail.DetailPanel;
+
+public class TestGamer extends StateMachineGamer {
+
+	@Override
+	public String getName() {
+		return "PGGGPPG Player";
+	}
+	
+	@Override
+	public StateMachine getInitialStateMachine() {
+		return new ProverStateMachine();
+	}
+	
+	/**
+	 * Selects the first legal move
+	 */
+	@Override
+	public Move stateMachineSelectMove(long timeout) throws MoveDefinitionException {
+		List<Move> moves = getStateMachine().getLegalMoves(getCurrentState(), getRole());
+		return moves.get(0);
+	}
+	
+	
+	/**
+	 * Does nothing for the metagame
+	 */
+	@Override
+	public void stateMachineMetaGame(long timeout) {
+		// Do nothing.
+	}
+
+
+
+	@Override
+	public void stateMachineStop() {
+		// Do nothing.
+	}
+
+	@Override
+	public void stateMachineAbort() {
+		// Do nothing
+	}
+
+	@Override
+	public DetailPanel getDetailPanel() {
+		return new ReflexDetailPanel();
+	}
+}
